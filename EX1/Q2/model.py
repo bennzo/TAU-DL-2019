8197ee732eab430789635be68abd43aae7d6610f
Ben Ohayon <bennohay@gmail.com>
2019-11-23 17:57:48 +0200

    Small changes in Q1 architecture and submitting Q2

---
+++ EX1/Q2/model.py
@@ -0,0 +1,52 @@
+import numpy as np
+import torch.nn as nn
+from torch.utils.data import Dataset
+from torch.nn.functional import relu, log_softmax
+from itertools import product
+from functools import reduce
+
+
+class IrisDataset(Dataset):
+    def __init__(self, datapath):
+        super().__init__()
+        self.iris_feats = np.genfromtxt(datapath, delimiter=',', dtype=float, usecols=range(0,4))
+        self.iris_specs = np.genfromtxt(datapath, delimiter=',', dtype=str, usecols=[4])
+
+        uniq_specs = np.unique(self.iris_specs)
+        spec_idx = dict(zip(uniq_specs, range(len(uniq_specs))))
+        self.labels = list(map(lambda spec: spec_idx[spec], self.iris_specs))
+        # self.labels = np.zeros((len(self.iris_specs), len(uniq_specs)), dtype=int)
+        # self.labels[range(len(self.iris_specs)), list(map(lambda spec: spec_idx[spec], self.iris_specs))] = 1
+
+    def __getitem__(self, idx):
+        return self.iris_feats[idx], self.labels[idx]
+
+    def __len__(self):
+        return len(self.labels)
+
+
+class IrisNet(nn.Module):
+    def __init__(self, insize=4, hsize=10, outsize=3):
+        super().__init__()
+        self.fc1 = nn.Linear(insize, hsize)
+        self.fc2 = nn.Linear(hsize, outsize)
+        self.activation = ReQU()
+        self.sm = nn.Softmax(dim=-1)
+
+    def forward(self, input):
+        x = self.fc1(input)
+        x = self.activation(x)
+        out = self.fc2(x)
+        probs = self.sm(out)
+        return out, probs
+
+class ReQU(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        return relu(x)*relu(x)
+
+
+if __name__ == '__main__':
+    dtest = IrisDataset('iris.data.csv')
